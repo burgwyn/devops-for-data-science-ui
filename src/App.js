96ac3bea5de90dc9b1e45db657f0e9d5c7851a45
Nat Burgwyn <burgwyn@gmail.com>
2019-03-20 22:19:17 -0400

    Add dropzone

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Dropzone from './Dropzone';
 
 class App extends Component {
   render() {
@@ -8,6 +9,14 @@ class App extends Component {
         <header className="App-header">
           <span>🧰</span>
         </header>
+        <main>
+          <section className="App-main">
+            <Dropzone />
+          </section>
+        </main>
+        <footer className="App-footer">
+          <span>🧰</span>
+        </footer>
       </div>
     );
   }
---
+++ src/Dropzone.js
@@ -0,0 +1,22 @@
+import React, {useCallback} from 'react'
+import {useDropzone} from 'react-dropzone'
+
+function Dropzone() {
+  const onDrop = useCallback(acceptedFiles => {
+    // Do something with the files
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
+  return (
+    <form {...getRootProps()}>
+      <input {...getInputProps()} />
+      {
+        isDragActive ?
+          <p>Drop the files here ...</p> :
+          <p>Drag 'n' drop some files here, or click to select files</p>
+      }
+    </form>
+  )
+}
+
+export default Dropzone;
